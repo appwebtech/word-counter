29bbf75969f1d04a6d8096fed71546cfcbec4729
Joseph <josembi@gmail.com>
2018-07-18 21:06:57 +0100

    Add Template Folder & Home File

--- neno/views.py
+++ neno/views.py
@@ -1,4 +1,8 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-	return HttpResponse('Hello')
+	return render(request, 'home.html')
+
+def josembi(request):
+	return HttpResponse('Django sucks cos it has very few dependencies!')
