fbbc0e82e373bdd2fe45d6eb01aa5451350c3775
Joseph <josembi@gmail.com>
2018-07-20 14:11:19 +0100

    Finish mini project

--- neno/views.py
+++ neno/views.py
@@ -1,8 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import operator
 
 def home(request):
 	return render(request, 'home.html')
+
+def about(request):
+	return render(request, 'about.html')
 
 def count(request):
 	fulltext = request.GET['fulltext']
@@ -19,5 +23,7 @@ def count(request):
 			# Add to dictionary
 			worddictionary[word] = 1
 
+	sortedwords = sorted(worddictionary.items(), key=operator.itemgetter(1))
 
-	return render(request, 'count.html',{'fulltext':fulltext, 'count':len(wordlist), 'worddictionary':worddictionary.items})
+
+	return render(request, 'count.html',{'fulltext':fulltext, 'count':len(wordlist), 'sortedwords':worddictionary.items})
